e272db7f5addea8a818f0eb404c60d0ac96030aa
Diptiranjan <iamdiptiranjan2004@gmail.com>
2023-07-14 20:53:16 +0530

    signup.html and signup.js modified (captcha Added)

--- src/signup/signup.js
+++ src/signup/signup.js
@@ -1,6 +1,39 @@
+// Captha code Starts
+
+let captcha;
+function generate() {
+
+	// Clear old input
+	document.getElementById("submit").value = "";
+
+	// Access the element to store
+	// the generated captcha
+	captcha = document.getElementById("image");
+	let uniquechar = "";
+
+	const randomchar =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	// Generate captcha for length of
+	// 5 with random character
+	for (let i = 1; i < 5; i++) {
+		uniquechar += randomchar.charAt(
+			Math.random() * randomchar.length)
+	}
+
+// Captcha code ends
+
+
+
 document.querySelector("#form").addEventListener("submit", signUp);
 function signUp(event) {
   event.preventDefault();
+
+  // for captcha 
+  const usr_input = document
+		.getElementById("submit").value;
+
+  
   const email = document.getElementById("email");
   const username = document.getElementById("name");
   const address = document.getElementById("address");
@@ -31,32 +64,62 @@ function signUp(event) {
 });
   }
   else{
- const dataobj = {
-    email: email.value,
-    name: username.value,
-    address: address.value,
-    mob: mob.value,
-    password: password.value,
-  };
-  fetch("http://localhost:7070/user", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(dataobj),
-  })
-    .then((res) =>{
-      res.json();
-      swal("Signin Successfull !!")
-    })
-    .then((data) => {
-        window.location.href = "../signin/signin.html";        
-      
-    });
+      if(usr_input ==+ captcha.innerHTML)
+      {
+        const dataobj = {
+          email: email.value,
+          name: username.value,
+          address: address.value,
+          mob: mob.value,
+          password: password.value,
+        };
+        fetch("http://localhost:7070/user", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(dataobj),
+        })
+          .then((res) =>{
+            res.json();
+            swal("Signin Successfull !!")
+          })
+          .then((data) => {
+              window.location.href = "../signin/signin.html";        
+            
+          });
+      }
+      else {
+          swal({
+              title: "Captcha Error !!",
+              text: "Enter the Correct Captcha !",
+              icon: "error",
+            });
+          generate();
+        }
   }
   })
-
-  
-
  
+}
+
+
+
+
+// For Captcha
+
+
+	// Store generated input
+	captcha.innerHTML = uniquechar;
+}
+
+function printmsg() {
+	
+
+	// Check whether the input is equal
+	// to generated captcha or not
+	if (usr_input == captcha.innerHTML) {
+		alert("Signed up successfully!")
+		//generate();
+	}
+	
 }
